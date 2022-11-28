f90517526455d3a5769dfd5f2fafffd8b893e1db
mrityunjay9693 <mkaykumar123@gmail.com>
2022-11-28 23:49:31 +0530

    var type Var_variableInto

---
+++ JavaScript/Var_variableInto.js
@@ -0,0 +1,23 @@
+/*
+    Type of variables in JS : 
+    a)var  
+    b)let  
+    c)const
+*/
+/* 
+    a)var: 
+*/
+var a;
+console.log('Variable a declared with var is always : '+a); //undefined
+
+a = 3;
+console.log('Value of a : '+a); // a = 3
+
+a = 'I am JavaScript.';
+console.log(a); // I am JavaScript.
+
+a = true;
+console.log(a); // true
+
+a = null;
+console.log(a); // null
