ef168214c2abc94b59b552902b6c0a861220bc6c
mrityunjay9693 <mkaykumar123@gmail.com>
2022-11-29 00:36:05 +0530

    for loop intro

---
+++ JavaScript/forLoop.js
@@ -0,0 +1,10 @@
+/* 
+    for loop syntax: 
+    for(variable_initialization; condition_is_checked; increment/decrement){
+            statement;
+    }
+
+*/
+for (i = 0; i <= 5; i++) {
+    console.log('value = '+i)
+}
