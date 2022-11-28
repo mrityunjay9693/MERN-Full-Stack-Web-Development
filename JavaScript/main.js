eeb3937eab26f27fdd6dd00725bf98b172aa857e
mrityunjay9693 <mkaykumar123@gmail.com>
2022-11-28 23:48:27 +0530

    js started

---
+++ JavaScript/main.js
@@ -0,0 +1,6 @@
+console.log("Welcome to tilde!")
+/*
+    console.log("Welcome to tilde!"); 
+    ->It is programmer choice to use semi-colon at the end of statements.
+*/
+console.log('Welcome node!');    
