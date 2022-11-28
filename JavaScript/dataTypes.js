1b19323cb2b0ab249dab49a991a4c37abdc7c899
mrityunjay9693 <mkaykumar123@gmail.com>
2022-11-28 23:51:14 +0530

    data types in JS

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,38 @@
+/* 
+    Data types in JS : a)Number b)String c)Boolean d)Undefined e)Null
+*/
+
+/* a)Number Data Type */ 
+var fno = 12;
+var Sno = 2.1;
+var tno = 201.1234
+console.log('a)Numbers data types: ')
+console.log(fno +' is integer');
+console.log(Sno +' is float')
+console.log(tno +' is in double\n');
+
+/* b) String Data Type: */
+console.log("b)String Data Type: ")
+var s1 = 't';
+var s2 = 'tilde';
+console.log("'"+s2+"'" +" is a string.");
+console.log("'"+s1+"'"+' is also a string\n');
+
+
+/* c)Boolean Data Types */
+var t = true;
+var f = false;
+console.log("c)Boolean Data Types: ");
+console.log('t: '+t, ' f: '+f+'\n');
+
+/* d)Undefined Data Types */
+var u;
+var u1 = undefined;
+console.log('d)Undefined Data Types: ');
+console.log('u: '+u)
+console.log('u1: '+u1+'\n');
+
+/* d)Null Data Types */
+var n = null;
+console.log('e)Null Data Types: ');
+console.log('n: '+n);
