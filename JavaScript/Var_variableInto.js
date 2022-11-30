a4bdd5f4487471b996d738a804adf5cacddd13a2
mrityunjay9693 <mkaykumar123@gmail.com>
2022-11-30 17:59:44 +0530

    var in js

--- JavaScript/Var_variableInto.js
+++ JavaScript/Var_variableInto.js
@@ -21,3 +21,29 @@ console.log(a); // true
 
 a = null;
 console.log(a); // null
+
+/*
+    ---------------------------------------------------------------------------------------------
+                                    Limitation of var type
+    ---------------------------------------------------------------------------------------------
+*/
+/*
+    a)Redeclarations: In below examples we have declared a variable 'Name' twice using var and 
+                      this redeclaration of 'Name' is one of the issue with var type. 
+*/
+var Name = 'I am from Muzaffarpur.'; //value in Name i.e., 'I am from Muzaffarpur is replaced'
+var Name = 'Bihar.';                // from this line i.e., 'Bihar' and output is : Bihar
+console.log(Name);
+
+/*
+    b)Scoping : var is function-scoped as well as  global-scope. 
+                variable with var type can be used outside the block-scope in which it is defined.
+                It is one of the limitation/issue.
+                Purpose/Objectives can easily expose of any program entity used with var as it can easily used outside
+                block-scope. 
+*/
+if(10 % 2 == 0){
+    var a = 'Divisible by 2';
+    console.log(a);
+}
+console.log(a) // a is defined in if-block but it can be used outside the scope where it is defined.
