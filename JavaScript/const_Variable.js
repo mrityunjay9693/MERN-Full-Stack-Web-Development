4776400073b1018f39ce174b49ca60f7e7d0f1ac
mrityunjay9693 <mkaykumar123@gmail.com>
2022-11-30 19:01:04 +0530

    const keyword

---
+++ JavaScript/const_Variable.js
@@ -0,0 +1,14 @@
+/* 
+    c)const: ->It is a keyword which is used to declare a variable.
+             ->Using const keyword with variable, once value assign to variable then its value will not
+               change throughout the program.
+             ->Re-declaration of variable and re-assigning of variable is not allowed.  
+*/
+
+const num = 10;   //value of num can not be altered further throughout the program.
+//console.log(num);
+
+//const num = 45;   //re-declarartion is not allowed.
+//console.log(num);  //Error in linme 10 : num is already declared in line 8 
+
+//a = 100;            //re-assigning is not allowed 
