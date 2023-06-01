bf7045a946285cfc2d2b2237611d7e0a6ea37f92
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-01 19:55:54 +0530

    mathematical operation program using function

---
+++ JavaScript/JSFunction/MathematicalOperation.js
@@ -0,0 +1,8 @@
+// Write a functon mathsOperation and do addition, substraction, multiply, divide for two numbers.
+function mathsOperation(a, b){
+    console.log(`Addition of a & b:`,a+b);
+    console.log(`Difference of a & b:`,a-b);
+    console.log(`Product of a & b:`,a*b);
+    console.log(`Division of a & b:`,a/b);
+}
+mathsOperation(12,10);
