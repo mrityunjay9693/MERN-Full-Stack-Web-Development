580416ad29ff503852d290067db777d0d05909dc
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-01 19:55:18 +0530

    addition program using function in JS

---
+++ JavaScript/JSFunction/additionTwoNumbers.js
@@ -0,0 +1,8 @@
+// Addition of two numbers using function addTwoNum by passing two numbers as parameters.
+
+function addTwoNum(a, b) {    // a and b are two parameters of the function which will accpet two numbers as inputs.
+    
+    console.log(`The addition of a and b = `, a + b );
+
+}
+addTwoNum(12,23); // 12, 23 are two arguments  
