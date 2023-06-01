df17f531543aec41d295516a3991630d303bbfec
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-01 19:56:28 +0530

    basic introduction to function in JS

---
+++ JavaScript/JSFunction/functionIntro.js
@@ -0,0 +1,16 @@
+/**
+ * "function" is a keyword used to create a function in JavaScript.
+ * Syntax : function function_name(parameters){ body fo the function }
+*/
+
+function greetFunction(){ //function definition or function declaration.
+
+    console.log("Hello World!"); // function Body (set of instruction).
+    //return "Hello World!"
+}
+
+greetFunction(); // function calling or invokation.
+//console.log(greetFunction());
+
+// functions in JavaScript are first citizens in the language.
+
