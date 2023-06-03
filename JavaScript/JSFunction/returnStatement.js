54fff46cd651704c726b28fc749457db01d64349
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-03 19:05:06 +0530

    understanding return statement in function

---
+++ JavaScript/JSFunction/returnStatement.js
@@ -0,0 +1,11 @@
+/**
+ * using return statement in a function.
+ */
+
+function greet() {
+    let greetMe = "Hello tilde!";
+    return greetMe;
+}
+//let returnGreet = greet();
+//console.log(returnGreet);
+console.log(greet());
