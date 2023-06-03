f5eb7ee45d4e30524cf24563a4240d38cd3ef8b0
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-03 19:27:10 +0530

    IIFE program

--- JavaScript/JSFunction/IIFE_Function.js
+++ JavaScript/JSFunction/IIFE_Function.js
@@ -1,20 +1,23 @@
 /**
  * IIFE : IIFE stands for Immediately Invoked Function Expression.
- * IIFE is a design pattern which is also known as Self-Executing Anonymous Function which consists of two major parts 
-   as follows:
+ * IIFE is a design pattern which is also known as Self-Executing Anonymous Function which consists of 
+   two major parts as follows:
 */
 
 //Ex : 1
 //Regular function as IIFE function
-let iife = (function(){
-    console.log("IIFE : Immediately invoked function expression.")
+let iife = (function () {
+  console.log("IIFE : Immediately invoked function expression.");
 })();
 
 //Ex : 2
 //Anonymous function as IIFE function.
-(function(a,b){
-    
-    console.log('Addition of a and b : ');
-    console.log(a+b);
+(function (a, b) {
+  console.log("Addition of a and b : ");
+  console.log(a + b);
+})(15, 87);
 
-})(15,87);
+let multiply = (function (num1, num2) {
+    return num1 * num2;
+})(20, 30);
+console.log(multiply);
