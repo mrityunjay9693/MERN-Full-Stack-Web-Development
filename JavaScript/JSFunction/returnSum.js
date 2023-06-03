a7d49ef97bda8a4b6737ea84aa2c00c56a7daa73
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-03 19:26:47 +0530

    returning sum of two numbers

---
+++ JavaScript/JSFunction/returnSum.js
@@ -0,0 +1,16 @@
+/**
+ * In this program, two number is added and the returning using return statement.
+ */
+function sumOperation() {
+  let num1 = 10;
+  let num2 = 20;
+  return num1 + num2;
+}
+//let adddition = sumOperation();
+//console.log(adddition);
+console.log("sum of num1 and num2:" + sumOperation());
+
+function sumNumber(num1, num2) {
+  return num1 + num2;
+}
+console.log(sumNumber(100, 200));
