b4c07f0d3d98da519a0b26c7f7b6a5678827de6b
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-04 14:34:48 +0530

    replacing values/element from the given array.

---
+++ JavaScript/JSArrays/replacingElementsInArray.js
@@ -0,0 +1,11 @@
+/**
+ * We can also replace an element or elements from an array from specified position(index).
+ */
+let studentName = ["Mrityunjay Kumar","Mrinal Sauraj","Vicky Raj","Sushma Swaraj"];
+console.log(studentName);
+
+studentName[2] = "Pratik";
+console.log(studentName); 
+
+studentName[0] = "Jyothi";
+console.log(studentName); 
