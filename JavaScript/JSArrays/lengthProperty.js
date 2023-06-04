8b01980b2608b1c2142b0f5be0ab09b15b5a5929
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-04 19:00:54 +0530

    length property of array.

---
+++ JavaScript/JSArrays/lengthProperty.js
@@ -0,0 +1,17 @@
+/**
+ * length of an array : To find the length of the given array, length property is used as shown below in the syntax:
+ * Syntax: arrayName.length;
+   Ex: let arr = [1 , 2 , 3 , 4 , 5 , 6 , 7 ];
+       len = arr.length;
+       console.log(len);
+    Output : 7 -> length of the given array 'arr'
+   -> It returns the length of the given array.
+ */
+
+let cityName = ["Mumbai", "Bangalore", "Pune", "Patna", "New Delhi"];
+len = cityName.length;
+console.log("Length of array cityName:");
+console.log(len);
+
+let sportName = ["Cricket", "Football", "Hockey", "Chess", "Shooting"];
+console.log("Length: "+sportName.length);
