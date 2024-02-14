2d6340d2b44bf9e35cb94b28f61eb02ede52f883
mrityunjay9693 <mkaykumar123@gmail.com>
2024-02-14 18:51:47 +0530

    more on iife function

--- JavaScript/JSFunction/IIFE_Function.js
+++ JavaScript/JSFunction/IIFE_Function.js
@@ -18,9 +18,35 @@ let iife = (function () {
 })(15, 87);
 
 let multiply = (function (num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 })(20, 30);
 
-//multiply(5,6); // An error will be here: multiply is not a function. Since it was alredy called at line 22 and the function ends
+//multiply(5,6); // An error will be here: multiply is not a function.
+//Since it was alredy called at line 22 and
+//the function ends
 
 console.log(multiply);
+
+let studentName = (function(){
+  let sName = "Mrityunjay Kumar"; 
+  console.log(sName);
+})();
+//the function will print value of sName.
+console.log(studentName); // undefined
+
+//Two different IIFE functions with the same name declared using the let keyword are not allowed.
+// let studentName = (function(){ //ERROR ; 'studentName' has alaready been declared
+//   let sName = "Mrinal Sauraj"; 
+//   console.log(sName);
+// })();
+
+//However, two different IIFE functions with the same name declared using the var keyword are allowed. 
+var myCar = (function(){
+  let myCar = "BMW";
+  console.log("My acr : "+myCar);
+})();
+
+var myCar = (function(){
+  let myCar = "Audi";
+  console.log("My acr : "+myCar);
+})();
