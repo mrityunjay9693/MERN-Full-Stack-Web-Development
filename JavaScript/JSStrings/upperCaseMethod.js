4d09e5e157fbc2f664e943be15e5ddd4de80da8d
mrityunjay9693 <mkaykumar123@gmail.com>
2024-03-04 20:02:52 +0530

    toUpperCase() method.

---
+++ JavaScript/JSStrings/upperCaseMethod.js
@@ -0,0 +1,7 @@
+/**
+ * toUpperCase() method: The toUpperCase() method converts each characters in the given string to its uppercase equivalent
+                         transforming the entire string into upper case characters.
+ * Syntax: string.toUpperCase();
+ */
+let myName = "Mrityunjay Kumar";
+console.log(myName.toUpperCase()); //MRITYUNJAY KUMAR
