7bcc5551a21fdc228d836be64bdcdf8b49f0235c
mrityunjay9693 <mkaykumar123@gmail.com>
2024-03-04 20:03:20 +0530

    toLowerCase() method.

---
+++ JavaScript/JSStrings/toLowerCaseMethod.js
@@ -0,0 +1,8 @@
+/**
+ * toLowerCase() method: The toLowerCase() method converts each characters in the given string to its lower case equivalent
+                         transforming the entire string into upper case characters. This means the method converts entire
+                         characters of given string into lower case i.e., string fully converted to lower case.
+ * Syntax: string.toLowerCase();
+ */
+let greet = "Good Morning, Welcome to TILDE";
+console.log(greet.toLowerCase());
